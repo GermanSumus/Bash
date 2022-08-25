422b95bc0f00a698b7d499d4437519cbd01a4e32
German Rodriguez <GermanRodriguezWork@Gmail.com>
2022-08-25 08:42:29 -0500

    Name is a simple scripts collecting user input

---
+++ Name.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# read input from user and save into variable "-p" allows you to deliver a prompt instead of a just a blinking cursor.
+read -p "Enter in your name: " name
+echo "Hello $name"
