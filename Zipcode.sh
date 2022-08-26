6aedb0698ae1d2598f3c015e718776700c3107f9
German Rodriguez <GermanRodriguezWork@Gmail.com>
2022-08-25 20:12:32 -0500

    added user validation to Zipcode to confirm only a 5 digit number is entered

--- Zipcode.sh
+++ Zipcode.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # reads user input to validate only a 5 digit zipcode is entered
-read -p "Enter in your zipcode: " zip
-if echo $zip | grep -E "^[0-9]{5}$" > /dev/null 2> /dev/null
-then
-	echo "Thank you for the correct zipcode"
-else
-	echo "Incorrect zip code"
-fi
+read -p "Enter in your zip code: " zip
+while echo $zip | grep -E -v "^[0-9]{5}$" > /dev/null 2> /dev/null
+do
+	echo "$zip is not a valid zip code"
+	echo "The zip code must be a 5 digit number."
+	read -p "Enter in a valid zip code: " zip
+done
+echo "Thank you for the correct zip code format"
